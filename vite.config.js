21a0cca0c1c94b042086df557521bc67bef63026
Brent Asbury <brentasbury@gmail.com>
2026-05-02 18:07:44 -0700

    Fix PWA manifest and icons

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ command }) => ({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
       manifest: {
         name: 'Plotter',
         short_name: 'Plotter',
@@ -19,13 +20,23 @@ export default defineConfig(({ command }) => ({
         start_url: '/plotter/',
         scope: '/plotter/',
         icons: [
-          { src: '/plotter/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/plotter/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
-          { src: '/plotter/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+          {
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
         ],
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallback: null,
       },
     }),
   ],
